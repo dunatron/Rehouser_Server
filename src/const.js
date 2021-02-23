b05c325e5b8fdc4b407a87481cdc2b38162f7d7f
Dunatron <heath.dunlop.hd@gmail.com>
2021-02-23 18:55:53 +1300

    ff

--- src/const.js
+++ src/const.js
@@ -48,6 +48,6 @@ exports.rehouserCookieOpt = () => {
     maxAge: JWT_TOKEN_MAX_AGE, // when the cookie expires
     sameSite: "None",
     secure: true, // connection needs to be over HTTPS
-    // httpOnly: false,
+    httpOnly: true,
   };
 };
